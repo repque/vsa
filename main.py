e8e716f05285739cc01b4533e474a121725eb08b
Gera Win <repque@yahoo.com>
2020-07-10 15:13:09 -0400

    fixed reverse

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from model import stocks, make_predictions
 
 all_stocks = set( stocks + ['TSLA','NFLX','HEAR'] )
 
-pred = sorted( list( make_predictions( all_stocks ) ), reversed=True )
+pred = sorted( list( make_predictions( all_stocks ) ), reverse=True )
 mail.send( body=pred )
